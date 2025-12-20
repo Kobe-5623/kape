4017dbab9fc6280c2cd5e4dd8f90bfbeb29fe9a1
Kobe Bennedict D. Tuazon <fiv5gamein@gmail.com>
2025-12-20 06:02:01 +0000

    making selection sort, not done

--- Kobe/selectionSort.c
+++ Kobe/selectionSort.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#define SIZE 100
 
-
-int* randNums(int size) {
-    int* nums = (int*)malloc(size * sizeof(int));
+int* randNums() {
+    int* nums = (int*)malloc(SIZE * sizeof(int));
 
     if (nums == NULL) {
         return NULL;
@@ -12,7 +12,7 @@ int* randNums(int size) {
 
     srand(time(NULL));
 
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < SIZE; i++) {
         int num = rand() % 100;
         nums[i] = num;
     }
@@ -20,9 +20,21 @@ int* randNums(int size) {
     return nums;
 }
 
+//Di pa po tapus..
+void sort() {
+    int *nums = randNums();
+    int min = 0;
+
+    for (int i = 0; i <= SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (nums[j] <= min || min == 0) {
+                min = j;
+            }
+        }
+    }
+}
+
 int main() {
-    int N = 100;
-    int *nums = randNums(N);
     /*
     for(int i = 0; i < N; i++) {
         printf("%d\n", nums[i]);
